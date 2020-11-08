6dae2890c5de49186b5f5a34ad9c37cba89b3c28
moddingfox <moddingfox@skulk.attlocal.net>
2020-11-08 22:44:43 +0000

    app/src/main/java/open/furaffinity/client/fragments/manageSiteSettings.java: Copied Template in app/src/main/java/open/furaffinity/client/fragments/manageUserSettings.java: Added logic to change settings app/src/main/java/open/furaffinity/client/fragments/profile.java: Unhide Site and User Settings app/src/main/java/open/furaffinity/client/pages/controlsSiteSettings.java: Copied Template app/src/main/java/open/furaffinity/client/pages/controlsUserSettings.java: Added logic to pull current settings app/src/main/res/layout/fragment_manage_site_settings.xml: Added layout for site settings app/src/main/res/layout/fragment_manage_user_settings.xml: Added layout for user settings

--- app/src/main/java/open/furaffinity/client/fragments/manageSiteSettings.java
+++ app/src/main/java/open/furaffinity/client/fragments/manageSiteSettings.java
@@ -1,22 +1,116 @@
 package open.furaffinity.client.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
 import open.furaffinity.client.R;
+import open.furaffinity.client.utilities.fabCircular;
+import open.furaffinity.client.utilities.kvPair;
+import open.furaffinity.client.utilities.uiControls;
+import open.furaffinity.client.utilities.webClient;
 
 public class manageSiteSettings extends Fragment {
+    private static String TAG = manageSiteSettings.class.getName();
+
+    private RadioButton disable_avatars_yes;
+    private RadioButton disable_avatars_no;
+    private RadioButton switch_date_format_full;
+    private RadioButton switch_date_format_fuzzy;
+    private Spinner select_preferred_perpage;
+    private Spinner select_newsubmissions_direction;
+    private Spinner select_thumbnail_size;
+    private RadioButton gallery_navigation_minigallery;
+    private RadioButton gallery_navigation_links;
+    private Spinner hide_favorites;
+    private Spinner no_guests;
+    private Spinner no_search_engines;
+    private Spinner no_notes;
+
+    private fabCircular fab;
+
+    private open.furaffinity.client.utilities.webClient webClient;
+    private open.furaffinity.client.pages.controlsSiteSettings page;
+
+    private void getElements(View rootView) {
+        disable_avatars_yes = rootView.findViewById(R.id.disable_avatars_yes);
+        disable_avatars_no = rootView.findViewById(R.id.disable_avatars_no);
+        switch_date_format_full = rootView.findViewById(R.id.switch_date_format_full);
+        switch_date_format_fuzzy = rootView.findViewById(R.id.switch_date_format_fuzzy);
+        select_preferred_perpage = rootView.findViewById(R.id.select_preferred_perpage);
+        select_newsubmissions_direction = rootView.findViewById(R.id.select_newsubmissions_direction);
+        select_thumbnail_size = rootView.findViewById(R.id.select_thumbnail_size);
+        gallery_navigation_minigallery = rootView.findViewById(R.id.gallery_navigation_minigallery);
+        gallery_navigation_links = rootView.findViewById(R.id.gallery_navigation_links);
+        hide_favorites = rootView.findViewById(R.id.hide_favorites);
+        no_guests = rootView.findViewById(R.id.no_guests);
+        no_search_engines = rootView.findViewById(R.id.no_search_engines);
+        no_notes = rootView.findViewById(R.id.no_notes);
+
+        fab = rootView.findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.ic_menu_save);
+    }
+
+    private void initClientAndPage() {
+        webClient = new webClient(requireContext());
+        page = new open.furaffinity.client.pages.controlsSiteSettings();
+    }
+
+    private void fetchPageData() {
+        page = new open.furaffinity.client.pages.controlsSiteSettings();
+        try {
+            page.execute(webClient).get();
+        } catch (ExecutionException | InterruptedException e) {
+            Log.e(TAG, "loadPage: ", e);
+        }
+    }
+
+    private void updateUIElements() {
+    }
+
+    private void updateUIElementListeners(View rootView) {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashMap<String, String> params = new HashMap<>();
+
+                try {
+                    new AsyncTask<webClient, Void, Void>() {
+                        @Override
+                        protected Void doInBackground(open.furaffinity.client.utilities.webClient... webClients) {
+                            webClients[0].sendPostRequest(open.furaffinity.client.utilities.webClient.getBaseUrl() + open.furaffinity.client.pages.controlsSiteSettings.getPagePath(), params);
+                            return null;
+                        }
+                    }.execute(webClient).get();
+                } catch (ExecutionException | InterruptedException e) {
+                    Log.e(TAG, "Could not update site settings: ", e);
+                }
+            }
+        });
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.not_implemented_yet, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_manage_site_settings, container, false);
+        getElements(rootView);
+        initClientAndPage();
+        fetchPageData();
+        updateUIElements();
+        updateUIElementListeners(rootView);
         return rootView;
     }
 }
---
+++ app/src/main/java/open/furaffinity/client/pages/controlsUserSettings.java
@@ -0,0 +1,102 @@
+package open.furaffinity.client.pages;
+
+import android.os.AsyncTask;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.HashMap;
+
+import open.furaffinity.client.utilities.webClient;
+
+public class controlsUserSettings extends AsyncTask<webClient, Void, Void> {
+    private static final String TAG = controlsUserSettings.class.getName();
+
+    private static String pagePath = "/controls/user-settings/";
+
+    private boolean accept_trades;
+    private boolean accept_commissions;
+    private HashMap<String, String> featured_journal_id;
+    private String featured_journal_id_current;
+
+    private String key;
+
+    public controlsUserSettings() {
+    }
+
+    private void processPageData(String html) {
+        Document doc = Jsoup.parse(html);
+
+        Elements inputs = doc.select("input[checked=checked]");
+        Elements selects = doc.select("select");
+        Element msgFormForm = doc.selectFirst("form[method=post][action=" + pagePath + "]");
+
+        if(inputs != null) {
+            for(Element input : inputs) {
+                switch(input.attr("name")) {
+                    case "accept_trades":
+                        accept_trades = ((input.attr("value").equals("1"))?(true):(false));
+                        break;
+                    case "accept_commissions":
+                        accept_commissions = ((input.attr("value").equals("1"))?(true):(false));
+                        break;
+                }
+            }
+        }
+
+        if(selects != null) {
+            for(Element select : selects) {
+                Element selected = select.selectFirst("option[selected]");
+                Elements options = select.select("option");
+                HashMap<String, String> newOptionList = new HashMap<>();
+
+                for(Element option : options) {
+                    if(option.hasAttr("value")) {
+                        newOptionList.put(option.attr("value"), option.text());
+                    }
+                }
+
+                switch (select.attr("name")) {
+                    case "featured_journal_id":
+                        featured_journal_id = newOptionList;
+                        featured_journal_id_current = ((selected != null)?(selected.attr("value")):("0"));
+                        break;
+                }
+
+            }
+        }
+
+        if(msgFormForm != null){
+            Element keyInput = msgFormForm.selectFirst("input[name=key]");
+            if(keyInput != null) {
+                key = keyInput.attr("value");
+            }
+        }
+    }
+
+    @Override
+    protected Void doInBackground(webClient... webClient) {
+        String html;
+        html = webClient[0].sendGetRequest(open.furaffinity.client.utilities.webClient.getBaseUrl() + pagePath);
+        processPageData(html);
+        return null;
+    }
+
+    public static String getPagePath() {
+        return pagePath;
+    }
+
+    public boolean getAcceptTrades() { return accept_trades; }
+    public boolean getAcceptCommissions() { return accept_commissions; }
+
+    public HashMap<String, String> getFeaturedJournalId() { return featured_journal_id; }
+    public String getFeaturedJournalIdCurrent() { return featured_journal_id_current; }
+
+    public String getKey() {
+        return key;
+    }
+}
