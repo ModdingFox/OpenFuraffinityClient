eec275a52d759d82c1ebd33892ae10b648eb93e8
moddingfox <moddingfox@skulk.attlocal.net>
2020-11-23 02:49:38 +0000

    Minor submitPageEdits

--- app/src/main/java/open/furaffinity/client/submitPages/submitControlsJournal.java
+++ app/src/main/java/open/furaffinity/client/submitPages/submitControlsJournal.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import java.util.HashMap;
 
 import open.furaffinity.client.abstractClasses.abstractPage;
-import open.furaffinity.client.pages.controlsJournal;
 
 public class submitControlsJournal extends abstractPage {
 
